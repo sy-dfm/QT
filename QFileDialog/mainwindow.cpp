47b8b34c14d142014cd338ff74be24fc86efa61f
DESKTOP-CFNI3AS\sy <vansy0975316291@gmail.com>
2021-07-14 16:23:33 +0700

    First commit

---
+++ QFileDialog/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString file_name = QFileDialog::getOpenFileName(this, "Open File", "C://");
+    QMessageBox::information(this, "title", file_name);
+}
